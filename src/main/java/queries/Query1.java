56bea8d377f055a870e32e7d2a3bd4aad70df13a
ceciliacal <54946836+ceciliacal@users.noreply.github.com>
2021-05-19 22:58:36 +0200

    creata tupla area_data_tot da file somm_latest per Query1

--- src/main/java/queries/Query1.java
+++ src/main/java/queries/Query1.java
@@ -4,8 +4,17 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.codehaus.janino.Java;
 import scala.Tuple2;
+import scala.Tuple3;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.Map;
 
 
@@ -14,22 +23,25 @@ public class Query1 {
     private static String filePath_puntiSommTipologia = "data/punti-somministrazione-tipologia.csv";
     private static String filePath_sommVacciniSummaryLatest = "data/somministrazioni-vaccini-summary-latest.csv";
 
-    public static <iter, tr> void main(String[] args){
+    public static void main(String[] args){
 
         SparkConf conf = new SparkConf()
-                .setMaster("local[*]")
+                .setMaster("local[1]")
                 .setAppName("Query1");
         JavaSparkContext sc = new JavaSparkContext(conf);
         System.out.println("ciao");
         sc.setLogLevel("ERROR");
 
 
+        Instant start = Instant.now();
+
         JavaRDD<String> lines_punti = sc.textFile(filePath_puntiSommTipologia);
+        lines_punti = removeHeader(lines_punti);
 
 
-        System.out.println("\nbefore map: ");
-        System.out.println(lines_punti.take(5));
+        System.out.println("\nlines_punti: "+lines_punti.take(5));
 
+        /*
         //prendo header
         String[] header = lines_punti.map(line -> line.split(",")).first();
         //lines_punti = lines_punti.filter(line -> !line.equals(header));
@@ -37,10 +49,13 @@ public class Query1 {
 
         System.out.println("regione: "+regione);
 
-        //tupla che ha <regioni, denominazione_struttura>
-        JavaRDD<Tuple2<String, String>> area_denomStrutt = preprocessDataset(lines_punti);
+         */
 
-        System.out.println("area_denomStrutt: "+area_denomStrutt.take(5));
+        //tupla che ha <regioni, denominazione_struttura>
+        //TRANSFORMATION !!!!
+        JavaRDD<Tuple2<String, String>> area_denomStrutt = preproc_countVaccCenters(lines_punti);
+
+        System.out.println("\narea_denomStrutt: "+area_denomStrutt.take(5));
 
 
         /*
@@ -64,15 +79,76 @@ public class Query1 {
          */
 
         JavaPairRDD.fromJavaRDD(area_denomStrutt);
+        //ACTION !!!!!
         Map<String, Long> counts = JavaPairRDD.fromJavaRDD(area_denomStrutt).countByKey();
-        System.out.println("counts"+counts);
+        System.out.println("\ncounts"+counts);
+
+
+        Instant end = Instant.now();
+        System.out.println("CON PARALLELISMO : Query completed in " + Duration.between(start, end).toMillis() + "ms");
+
+        //inizio 2 parte della query (con file somministrazioni-latest)
+        JavaRDD<String> lines_somm = sc.textFile(filePath_sommVacciniSummaryLatest);
+        lines_somm = removeHeader(lines_somm);
+
+        /*
+
+        //JavaRDD<String> lines_somm2 = lines_somm.filter(line -> line.);
+        String[] header2 = lines_somm.map(line -> line.split(",")).first();
+        System.out.println("\n\n header2:         "+ Arrays.toString(header2));
+        JavaRDD<String> lines_somm2  = lines_somm.filter(line -> line.contains("data_somministrazione") == false);
+
+         */
+
+
+
+
+        System.out.println("\n\nlines_somm: "+lines_somm.take(5));
+
+        JavaRDD<Tuple3<String, LocalDate, Integer>> area_data_tot = preproc_query1(lines_somm);
+        System.out.println("area_data_tot: "+area_data_tot.take(5));
+
+
 
 
 
     }
+    /*
+    per prendere singola colonna:
+        JavaRDD<String> col_regioni= lines_punti.map(line -> line.split(",")[0]);
+     */
+
+    public static JavaRDD<String> removeHeader(JavaRDD<String> lines) {
+
+        String[] header = lines.map(line -> line.split(",")).first();
+        //System.out.println("\n\n header:"+ Arrays.toString(header2));
+        JavaRDD<String> lines_noHeader  = lines.filter(line -> line.contains(header[0]) == false);
+
+        return lines_noHeader;
+
+    }
+
+    public static JavaRDD<Tuple3<String, LocalDate, Integer>> preproc_query1(JavaRDD<String> lines) {
+
+        JavaRDD<Tuple3<String, LocalDate, Integer>> result = lines.map( row -> {
+
+            String[] myFields = row.split(",");
+            String col_area = myFields[1];
+            //LocalDate col_date = stringToDate(row.split(",")[0]);
+            LocalDate col_date = LocalDate.parse(myFields[0]);
+            Integer col_tot =  Integer.parseInt(myFields[2]);
+
+            return new Tuple3<>(col_area, col_date, col_tot);
+        });
+
+
+
+        return result;
+    }
+    
 
     //preproc punti somministrazioni x regione (devo contare numero punti somm x ogni regione)
-    private static JavaRDD<Tuple2<String, String>> preprocessDataset(JavaRDD<String> lines_punti) {
+    public static JavaRDD<Tuple2<String, String>> preproc_countVaccCenters(JavaRDD<String> lines_punti) {
 
         JavaRDD <Tuple2<String, String>> result = lines_punti.map(element -> {
                     String[] myFields = element.split(",");
