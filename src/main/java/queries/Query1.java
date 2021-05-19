e442e88f04224eb6300ff3d04ebe59c4d5d3d820
ceciliacal <54946836+ceciliacal@users.noreply.github.com>
2021-05-20 00:52:49 +0200

    fatto area_data_tot con kv, da fare la somma e media

--- src/main/java/queries/Query1.java
+++ src/main/java/queries/Query1.java
@@ -4,17 +4,12 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.codehaus.janino.Java;
 import scala.Tuple2;
 import scala.Tuple3;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.Map;
 
 
@@ -44,7 +39,6 @@ public class Query1 {
         /*
         //prendo header
         String[] header = lines_punti.map(line -> line.split(",")).first();
-        //lines_punti = lines_punti.filter(line -> !line.equals(header));
         String regione = header [0];
 
         System.out.println("regione: "+regione);
@@ -53,7 +47,7 @@ public class Query1 {
 
         //tupla che ha <regioni, denominazione_struttura>
         //TRANSFORMATION !!!!
-        JavaRDD<Tuple2<String, String>> area_denomStrutt = preproc_countVaccCenters(lines_punti);
+        JavaRDD<Tuple2<String, String>> area_denomStrutt = centersPerArea(lines_punti);
 
         System.out.println("\narea_denomStrutt: "+area_denomStrutt.take(5));
 
@@ -64,14 +58,6 @@ public class Query1 {
 
          */
 
-        // adesso devo eliminare la prima riga
-
-
-                /*(idx, iter) ->{
-              if (idx == 0) iter.drop(1) else iter;
-        });
-
-                 */
 
         // poi contare il num di strutture per regione
         /* cioè devo creare nuovo RDD dove la chiave è la regione e il value è il numero di strutture
@@ -87,26 +73,20 @@ public class Query1 {
         Instant end = Instant.now();
         System.out.println("CON PARALLELISMO : Query completed in " + Duration.between(start, end).toMillis() + "ms");
 
-        //inizio 2 parte della query (con file somministrazioni-latest)
-        JavaRDD<String> lines_somm = sc.textFile(filePath_sommVacciniSummaryLatest);
-        lines_somm = removeHeader(lines_somm);
-
         /*
-
-        //JavaRDD<String> lines_somm2 = lines_somm.filter(line -> line.);
-        String[] header2 = lines_somm.map(line -> line.split(",")).first();
-        System.out.println("\n\n header2:         "+ Arrays.toString(header2));
-        JavaRDD<String> lines_somm2  = lines_somm.filter(line -> line.contains("data_somministrazione") == false);
-
+        ====================== inizio 2 parte della query (con file somministrazioni-latest) ===========================
          */
 
-
+        JavaRDD<String> lines_somm = sc.textFile(filePath_sommVacciniSummaryLatest);
+        lines_somm = removeHeader(lines_somm);
 
 
         System.out.println("\n\nlines_somm: "+lines_somm.take(5));
 
-        JavaRDD<Tuple3<String, String, Integer>> area_data_tot = preproc_query1(lines_somm);
+        JavaPairRDD<Tuple2<String, String>, Integer> area_data_tot = monthlyVaccinesPerArea(lines_somm);
         System.out.println("area_data_tot: "+area_data_tot.take(9));
+
+
 
 
 
@@ -116,19 +96,6 @@ public class Query1 {
         JavaRDD<String> col_regioni= lines_punti.map(line -> line.split(",")[0]);
      */
 
-    /*
-    public static void parseDate (LocalDate myDate){
-        LocalDate first = new LocalDate(2021,01,01);
-        LocalDate last = new LocalDate(2021,01,01);
-
-        if myDate.isAfter(2021-01-01)
-
-
-
-
-    }
-
-     */
 
     public static JavaRDD<String> removeHeader(JavaRDD<String> lines) {
 
@@ -140,9 +107,10 @@ public class Query1 {
 
     }
 
+
     //preproc somministrazioni vaccini summary latest, per prendere le colonne area, data somm, totale
     //da mettere in una tupla x processare i dati dopo
-    public static JavaRDD<Tuple3<String, String, Integer>> preproc_query1(JavaRDD<String> lines) {
+    public static JavaPairRDD<Tuple2<String, String>, Integer> monthlyVaccinesPerArea(JavaRDD<String> lines) {
 
         LocalDate firstDay = LocalDate.parse("2021-01-01");
         LocalDate lastDay = LocalDate.parse("2021-05-31");
@@ -150,27 +118,44 @@ public class Query1 {
         JavaRDD<Tuple3<String, LocalDate, Integer>> resultWithLocalDate = lines.map( row -> {
 
             String[] myFields = row.split(",");
-            String col_area = myFields[1];
             LocalDate col_date = LocalDate.parse(myFields[0]);
+            String col_area = myFields[1];
             Integer col_tot =  Integer.parseInt(myFields[2]);
 
             return new Tuple3<>(col_area, col_date, col_tot);
         }).filter( line -> line._2().isAfter(firstDay) && line._2().isBefore(lastDay));
 
-        JavaRDD<Tuple3<String, String, Integer>> result = resultWithLocalDate.map(line ->
-            new Tuple3<>(line._1(), line._2().getMonth().toString(), line._3()));
+        JavaPairRDD<Tuple2<String, String>, Integer> res = resultWithLocalDate.mapToPair( line ->
+                new Tuple2<> ( new Tuple2<>(line._1(), line._2().getMonth().toString() ), line._3()));
+
+        return res;
+    }
+
+    /*
+    //preproc punti somministrazioni x regione (devo contare numero punti somm x ogni regione)
+    public static JavaPairRDD<String, String> centersPerArea2(JavaRDD<String> lines_punti) {
+
+        JavaPairRDD<String, String> result = lines_punti.mapToPair(element -> {
+                    String[] myFields = element.split(",");
+                    String col_reg = myFields[0];
+                    String col_denom = myFields[1];
+                    new Tuple2<>(col_reg,col_denom);
+             }
+
+        );
 
 
 
-        //result = result.filter( line -> line._2().isAfter(firstDay) && line._2().isBefore(lastDay) == true);
-
-
+        //System.out.println("RDD Tuple2 "+ result.take(5));
         return result;
     }
+    
+     */
+
 
 
     //preproc punti somministrazioni x regione (devo contare numero punti somm x ogni regione)
-    public static JavaRDD<Tuple2<String, String>> preproc_countVaccCenters(JavaRDD<String> lines_punti) {
+    public static JavaRDD<Tuple2<String, String>> centersPerArea(JavaRDD<String> lines_punti) {
 
         JavaRDD <Tuple2<String, String>> result = lines_punti.map(element -> {
                     String[] myFields = element.split(",");
@@ -178,9 +163,8 @@ public class Query1 {
                     String col_denom = myFields[1];
                     return new Tuple2<>(col_reg,col_denom);
                 }
-
-
         );
+
 
         //System.out.println("RDD Tuple2 "+ result.take(5));
         return result;
