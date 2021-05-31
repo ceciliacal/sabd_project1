4a02aa93d8f194b012b0ef4a9ec638e0e368850b
ceciliacal <54946836+ceciliacal@users.noreply.github.com>
2021-05-31 19:22:14 +0200

    updated uploadCsv.sh

--- docker/uploadCsv.sh
+++ docker/uploadCsv.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-sudo docker cp ./csv hdfs-namenode:/csv/
-sudo docker exec -it hdfs-namenode hdfs dfs -put /cartella /cartella
+CSV1="https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/punti-somministrazione-tipologia.csv"
+CSV2="https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-latest.csv"
+CSV3="https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-summary-latest.csv"
+
+mkdir -m 777 data
+cd ./data
+
+wget $CSV1
+wget $CSV2
+wget $CSV3
+
+cd ..
+
+sudo docker cp data hdfs-namenode:/data/
+sudo docker exec -it hdfs-namenode hdfs dfs -put /data /data
